dc155c620ab34e466b3f116b018a1e34ace56feb
Keerti Koya <koyakeerti@gmail.com>
2024-09-18 10:30:40 -0700

    9/18/24 methods

---
+++ Methods.java
@@ -0,0 +1,29 @@
+public class Methods {
+    public static void main(String[] args) {
+        method1();
+        add(9,8);
+        int a = 7;
+        int b = 11;
+        subtract(b,a); // ORDER MATTERS: will do 11-4
+        System.out.println(multiply(a, b)); // prints c method that was returned
+    }
+
+    public static void method1(){ // will only run when called by main method
+        int x = 5;
+        System.out.println("hello period 1");
+        System.out.println(x+x+x);
+    }
+
+    public static void add(int a, int b){
+        System.out.println(a+b);
+    }
+
+    public static void subtract(int a, int b){
+        System.out.println(a-b);
+    }
+
+    public static int multiply(int a, int b){ // will only return an INT
+        int c = a*b;
+        return(c); // sends c back to main method
+    }
+}
