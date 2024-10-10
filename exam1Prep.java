1ffe7a2d6c997aa579b0dfd0ca46d06728df82f9
Keerti Koya <koyakeerti@gmail.com>
2024-10-10 08:45:15 -0700

    exam 1 prep

---
+++ exam1Prep.java
@@ -0,0 +1,8 @@
+public class exam1Prep {
+    public static void main(String[] args) {
+        System.out.println("\\\\\\\\\\\\\\\\\"");
+        System.out.println("world\thi    awesome\tcool\tnice");
+
+        
+    }
+}
