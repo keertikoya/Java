51b1370f66f7995b87c0228ebeccb4bdca9e5760
Keerti Koya <koyakeerti@gmail.com>
2024-10-15 08:59:50 -0700

    10/14/24 string manipulation

---
+++ StringManipulation.java
@@ -0,0 +1,26 @@
+// Keerti Koya
+// Period 1
+// 10/15/24
+
+public class StringManipulation {
+    public static void main(String[] args) {
+        String x = "hello world";
+
+        // substring
+        System.out.println(x.substring(3)); // lo world
+        System.out.println(x.substring(3,6)); // lo
+        System.out.println(x.substring(0,8)); // hello wo
+
+        // length
+        System.out.println(x.length()); // 11
+
+        // indexOf
+        System.out.println(x.indexOf("l")); // 2
+
+        // compareTo
+        String y = "hi world";
+        String z = "hello world";
+        System.out.println(x.compareTo(y)); // prints -4 because the first difference is i/e. they are 4 chars apart.
+        System.out.println(x.compareTo(z)); // prints 0 because they are fully equal
+     }
+}
