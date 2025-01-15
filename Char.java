2f1ee7fa9e7eb53e4dd3a09e3f4934cbed3191c6
Keerti Koya <koyakeerti@gmail.com>
2025-01-15 08:51:23 -0800

    1/15/25 char lecture

---
+++ Char.java
@@ -0,0 +1,12 @@
+public class Char {
+    public static void main(String[] args) {
+        String x = "mike12323";
+        char a = x.charAt(3);
+        System.out.println(a);
+
+        char y = 'K';
+        y += 3031;
+        System.out.println(y);
+        System.out.println((int)y);
+    }
+}
